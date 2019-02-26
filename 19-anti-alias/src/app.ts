417f085e806236c42613a0d1385c0094ec20d9ab
inuyasha0618 <chenjiandiwang@qq.com>
2019-02-26 16:40:21 +0800

    add 19

---
+++ 19-anti-alias/src/app.ts
@@ -0,0 +1,63 @@
+import { vec3, mat4 } from 'gl-matrix';
+import RenderLooper from 'render-looper';
+import { getContext, fetchObjFile, resizeCvs2Screen, getRadian } from './utils/index';
+import { ShaderProgram, Mesh, loadTex, OrbitCamera } from './gl-helpers/index';
+import phongVertSrc from './shaders/phongVert';
+import phongFragSrc from './shaders/phongFrag';
+
+const gl: WebGL2RenderingContext = getContext('#cvs');
+
+// 获得屏幕尺寸
+
+// 创建shaderprogram
+const phongShaderProgram: ShaderProgram = new ShaderProgram(gl, phongVertSrc, phongFragSrc, 'phongShaderProgram');
+phongShaderProgram.use();
+phongShaderProgram.uniform1i('tex', 0);
+phongShaderProgram.uniform3fv('light_direction', vec3.fromValues(-2, 1, -1));
+
+const { width: SCR_WIDTH, height: SCR_HEIGHT } = resizeCvs2Screen(gl);
+
+// 加载楼房所使用的高光贴图
+const buildingTexture: WebGLTexture = gl.createTexture();
+loadTex('../images/strip2.jpg', buildingTexture, gl);
+
+// 创建楼体mesh
+let buildingMesh: Mesh = new Mesh();
+fetchObjFile('../models/Tencent_BinHai.obj').then(function({vertPosArray, vertUVArray, vertNormArray, vertIndexArray}) {
+    buildingMesh.init(gl, vertPosArray, vertUVArray, vertNormArray, vertIndexArray);
+})
+
+// 创建相机
+const camera: OrbitCamera = new OrbitCamera(gl, 15, 0, -25, SCR_WIDTH / SCR_HEIGHT);
+gl.viewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+gl.enable(gl.DEPTH_TEST);
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+
+const uvSpeed: number = -0.1;
+let uvOffset: number = 0;
+function drawCB(msDt: number): void {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    uvOffset += uvSpeed * msDt * 0.001;
+    const view: mat4 = camera.getViewMatrix();
+    const perspective: mat4 = camera.getPerspectiveMatrix();
+    const model: mat4 = mat4.create();
+    mat4.translate(model, model, [0, -6.0, 0]);
+    mat4.rotateY(model, model, getRadian(-45));
+    mat4.scale(model, model, [0.25, 0.25, 0.25]);
+    phongShaderProgram.use();
+    phongShaderProgram.uniformMatrix4fv('uModel', model);
+    phongShaderProgram.uniformMatrix4fv('uView', view);
+    phongShaderProgram.uniformMatrix4fv('uPerspective', perspective);
+    phongShaderProgram.uniform3fv('viewPos', camera.position);
+    phongShaderProgram.uniform1f('yOffset', uvOffset);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, buildingTexture);
+
+    buildingMesh.draw();
+}
+
+
+const looper = new RenderLooper(drawCB).start();
---
+++ 19-anti-alias/src/gl-helpers/Mesh.ts
@@ -0,0 +1,55 @@
+export default class Mesh {
+    private initDone: boolean = false;
+    gl: WebGL2RenderingContext;
+    VAO: WebGLVertexArrayObject;
+    IBO: WebGLBuffer;
+    counts: number;
+    init(gl: WebGL2RenderingContext, posArr: Array<number>, uvArr: Array<number>, normArr: Array<number>, idxArr: Array<number>) {
+        this.VAO = gl.createVertexArray();
+        gl.bindVertexArray(this.VAO);
+
+        const posVBO: WebGLBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, posVBO);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(posArr), gl.STATIC_DRAW);
+        gl.enableVertexAttribArray(0);
+        gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+
+        if (normArr.length) {
+            const normVBO: WebGLBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, normVBO);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normArr), gl.STATIC_DRAW);
+            gl.enableVertexAttribArray(1);
+            gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, 0);
+        }
+
+        if (uvArr.length) {
+            const uvVBO: WebGLBuffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, uvVBO);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvArr), gl.STATIC_DRAW);
+            gl.enableVertexAttribArray(2);
+            gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 0, 0);
+        }
+
+        const IBO: WebGLBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, IBO);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(idxArr), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+        gl.bindVertexArray(null);
+
+        this.gl = gl;
+        this.IBO = IBO;
+        this.counts = idxArr.length;
+        this.initDone = true;
+    }
+
+    draw(): void {
+        if (!this.initDone) return;
+        const gl: WebGL2RenderingContext = this.gl;
+        gl.bindVertexArray(this.VAO);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IBO);
+        gl.drawElements(gl.TRIANGLES, this.counts, gl.UNSIGNED_INT, 0);
+        gl.bindVertexArray(null);
+    }
+}
