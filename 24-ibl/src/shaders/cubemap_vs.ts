73b83a69a42182acbe431c9eb977f8b6d7d3e470
inuyasha0618 <chenjiandiwang@qq.com>
2019-03-12 18:11:27 +0800

    add shaders

---
+++ 24-ibl/src/shaders/cubemap_vs.ts
@@ -0,0 +1,17 @@
+const src: string = `
+#version 300 es
+layout (location = 0) in vec3 aPos;
+
+out vec3 WorldPos;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+void main()
+{
+    WorldPos = aPos;  
+    gl_Position =  projection * view * vec4(WorldPos, 1.0);
+}
+`.trim();
+
+export default src;
